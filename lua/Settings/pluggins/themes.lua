8a7df18d36ed9844e007ad7419ce1e70b37871ab
Adrian <A01198211@tec.mx>
2023-10-24 20:09:04 -0600

    nord theme

--- lua/Settings/pluggins/themes.lua
+++ lua/Settings/pluggins/themes.lua
@@ -1,72 +1,85 @@
 function setTransparent()
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
 
-	{
-		"projekt0n/github-nvim-theme",
-		tag = "v0.0.7",
-		-- config = function()
-		--     vim.cmd('colorscheme github_dimmed')
-		-- end
-		keys = "<leader>pc",
-	},
+    {
+        "projekt0n/github-nvim-theme",
+        tag = "v0.0.7",
+        -- config = function()
+        --     vim.cmd('colorscheme github_dimmed')
+        -- end
+        keys = "<leader>pc",
+    },
 
-	-- onedark
-	{
-		"navarasu/onedark.nvim",
-		config = function()
-			require("onedark").setup({
-				-- style = 'cool'
-			})
-			vim.cmd("colorscheme onedark")
-			-- setTransparent()
-		end,
-		-- keys = "<leader>pc"
-	},
-	-- vscode
-	{
-		"Mofiqul/vscode.nvim",
-		keys = "<leader>pc",
-	},
+    -- vscode
+    {
+        "Mofiqul/vscode.nvim",
+        keys = "<leader>pc",
+    },
 
-	-- tokyo night
-	{
-		"folke/tokyonight.nvim",
+    -- tokyo night
+    {
+        "folke/tokyonight.nvim",
 
-		-- config = function()
-		--     vim.cmd("colorscheme tokyonight-night")
-		--     -- setTransparent()
-		-- end
-		keys = "<leader>pc",
-	},
-	{
-		"Mofiqul/dracula.nvim",
-		-- config = function()
-		--     vim.cmd('colorscheme dracula')
-		-- end,
-		keys = "<leader>pc",
-	},
-	-- nord
-	{
-		"shaunsingh/nord.nvim",
-		--[[ config = function()
+    },
+    -- onedark
+    {
+        "navarasu/onedark.nvim",
+        -- config = function()
+        --     require('onedark').setup {
+        --
+        --         -- style = 'cool'
+        --     }
+        --     vim.cmd('colorscheme onedark')
+        --     -- setTransparent()
+        -- end
+        -- keys = "<leader>pc"
+    },
+    -- vscode
+    {
+        'Mofiqul/vscode.nvim',
+        keys = "<leader>pc"
+    },
+
+
+    -- tokyo night
+    {
+        'folke/tokyonight.nvim',
+
+        -- config = function()
+        --     vim.cmd("colorscheme tokyonight-night")
+        --     -- setTransparent()
+        -- end
+        keys = "<leader>pc"
+    },
+    {
+        'Mofiqul/dracula.nvim',
+        -- config = function()
+        --     vim.cmd('colorscheme dracula')
+        -- end,
+        keys = "<leader>pc"
+    },
+    -- nord
+    {
+        'shaunsingh/nord.nvim',
+        config = function()
             vim.cmd('colorscheme nord')
-        end ]]
-		keys = "<leader>pc",
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		config = function()
-			--vim.cmd('colorscheme gruvbox')
-		end,
-		keys = "<leader>pc",
-	},
+        end,
+        -- keys = "<leader>pc",
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        config = function()
+            --vim.cmd('colorscheme gruvbox')
+        end,
+        keys = "<leader>pc",
+    },
 
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-	},
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+    },
 }
